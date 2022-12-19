f7d28e7ac7660287ba9d44f7bbcd2c7a7335c4c1
Rawan Alharthi <s43800023@students.tu.edu.sa>
2022-12-19 09:58:40 +0300

    Initial Commit

---
+++ ProfileCard/ProfileCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileCardApp.swift
+//  ProfileCard
+//
+//  Created by 라완 💕 on 25/05/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct ProfileCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
