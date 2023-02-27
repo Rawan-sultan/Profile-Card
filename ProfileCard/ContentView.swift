aee23f21d31cf65c6892c51a38b4fb2f793828ce
Rawan Alharthi <s43800023@students.tu.edu.sa>
2023-02-27 03:31:19 +0300

    Profile-Card Assignment

--- ProfileCard/ContentView.swift
+++ ProfileCard/ContentView.swift
@@ -10,12 +10,36 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            ZStack{
+                Circle()
+                    .fill(Color.yellow)
+                    .frame(width: 250, height: 250)
+                    Image("panda")
+                    
+                    .resizable()
+                    .frame(width: 230.0, height: 230.0)
+                    .cornerRadius(150)
+            }
+            
+            Text("Rawan Alharthi")
+                .font(Font.custom("CourierNewPS-BoldItalicMT", size: 35))
+                .padding()
+
+                HStack{
+                  Image(systemName: "mail.fill")
+                        .resizable()
+                        .frame(width: 30, height: 30)
+                    Text("Rawansultan1999@gmail.com")
+                        .font(Font.custom("Charter-Roman", size: 20))
+                    .accentColor(.black)
+                }
+                .padding()
+                .background(Color.yellow)
+                .cornerRadius(50)
+          
         }
-        .padding()
+        .frame(maxWidth: .infinity, maxHeight: 750)
+        .background(Color.gray)
     }
 }
 
